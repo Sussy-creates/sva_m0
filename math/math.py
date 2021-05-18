ba7b8fccaf79c497abd90a02455902d2e7e7edcb
aidan <aidansuss@gmail.com>
2021-05-19 07:58:17 +1000

    My First Commit!

---
+++ math/math.py
@@ -0,0 +1,17 @@
+# Primative Types
+print("Hello Cunt!") # String
+print(15) # Integers (Number without decimals)
+print(15.5432) # Float (Numbers with decimals)
+print(34 + 23j) # Complex Number (Real + Imaginary)
+print(True) # Boolean (True/False only)
+
+# Operators
+print("5 + 5 =", 5 + 5) #10
+print("5 - 5 =", 5 - 5) #0
+print("5 * 5 =", 5 * 5) #25
+print("5 / 5 =", 5 / 5) #1
+
+print("5**2 =", 5**2) # Exponent / Power
+print("5 // 2 =", 5 // 2) # Integer Division (rownds down to nearest whole number)
+print("4 % 2 =", 4 % 2) # Modulus (returns the remainder of the first argument divided by the second)
+
