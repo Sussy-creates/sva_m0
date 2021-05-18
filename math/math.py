446d282c445f04872288f889078cc81c2991fba9
aidan <aidansuss@gmail.com>
2021-05-19 08:30:54 +1000

    Replaced hard-coded values with variables

--- math/math.py
+++ math/math.py
@@ -1,17 +1,19 @@
 # Primative Types
-print("Hello Cunt!") # String
+subject = "Bob"
+print(f"Hello {subject}!") # String
 print(15) # Integers (Number without decimals)
 print(15.5432) # Float (Numbers with decimals)
 print(34 + 23j) # Complex Number (Real + Imaginary)
 print(True) # Boolean (True/False only)
 
 # Operators
-print("5 + 5 =", 5 + 5) #10
-print("5 - 5 =", 5 - 5) #0
-print("5 * 5 =", 5 * 5) #25
-print("5 / 5 =", 5 / 5) #1
+num = 8 
+print(f"{num} + {num} =", num + num) #10
+print(f"{num} - {num} =", num - num) #0
+print(f"{num} * {num} =", num * num) #25
+print(f"{num} / {num} =", num / num) #1
 
-print("5**2 =", 5**2) # Exponent / Power
-print("5 // 2 =", 5 // 2) # Integer Division (rownds down to nearest whole number)
+print(f"{num}**2 =", num**2) # Exponent / Power
+print(f"{num} // 2 =", num // 2) # Integer Division (rownds down to nearest whole number)
 print("4 % 2 =", 4 % 2) # Modulus (returns the remainder of the first argument divided by the second)
 
